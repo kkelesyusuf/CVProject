9b06c3b27d69a1373071dcab0b0c810878616570
kkelesyusuf <kkelesyusuf@gmail.com>
2023-07-03 12:46:52 +0300

    Add project files.

---
+++ CV/Controllers/CertificateController.cs
@@ -0,0 +1,33 @@
+﻿using CV.Models.Entity;
+using CV.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class CertificateController : Controller
+    {
+        // GET: Certificate
+        DBCvEntities dB = new DBCvEntities();
+        GenericRepository<Certificates> repository = new GenericRepository<Certificates>();
+        public ActionResult Index()
+        {
+            var values = repository.GetList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult UpdateCertificate(int id)
+        {
+            var certificate = repository.GetByID(x => x.ID == id);
+            return View(certificate);
+        }
+        [HttpPost]
+        public ActionResult UpdateCertificate(Certificates t)
+        {
+            return View();
+        }
+    }
+}
